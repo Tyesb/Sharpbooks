81b065376ccf9e3f91fbcc226c8dcd74f8e400c7
ADukeLabs <d.alex.duke@gmail.com>
2014-12-01 12:11:03 +1300

    Books model added

---
+++ SrcCode/SharpBooks/SharpBooks/Models/Book.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SharpBooks.Models
+{
+    public class Book
+    {
+        public string Title { get; set; }
+    }
+}
