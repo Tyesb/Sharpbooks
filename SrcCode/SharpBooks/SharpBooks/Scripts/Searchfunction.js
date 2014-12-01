e831541637c74165152869422a662f143ac50bcb
Tyesb <tbennet@hotmail.co.nz>
2014-12-01 17:15:01 +1300

    returns db and shows titles

--- SrcCode/SharpBooks/SharpBooks/Scripts/Searchfunction.js
+++ SrcCode/SharpBooks/SharpBooks/Scripts/Searchfunction.js
@@ -1,1 +1,24 @@
-﻿$(document)
+﻿$(function () {
+    $('#submitBookSearch').on('click', function (e) {
+        e.preventDefault();
+        console.log('coffee');
+        $.ajax({
+            url: '/api/Search',
+            data: { search: $('#Search').val() },
+            type: 'GET',
+            datatType: 'json',
+            success: function (data) {
+                console.log(data);
+                $.each(data, function(index, value) {
+                    $('#results').append('<li>' + value.Title + '</li>');
+
+                });
+            },
+            fail:  function() {
+                alert('something fucked up');
+            }
+
+        });
+
+    });
+});
