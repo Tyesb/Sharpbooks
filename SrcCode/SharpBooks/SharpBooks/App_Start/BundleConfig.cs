e895bd79116f7b0b1442f6402c9cec1156971e96
Tyesb <tbennet@hotmail.co.nz>
2014-12-02 16:22:50 +1300

    Revert "Js"

--- SrcCode/SharpBooks/SharpBooks/App_Start/BundleConfig.cs
+++ SrcCode/SharpBooks/SharpBooks/App_Start/BundleConfig.cs
@@ -14,10 +14,6 @@ namespace SharpBooks
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/tyebun").Include(
-                        "~/Scripts/Searchfunction.js"));
-            
-
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
