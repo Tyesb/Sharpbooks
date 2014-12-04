80949307974df37efff1d9d05768861d9fffd490
Tyesb <tbennet@hotmail.co.nz>
2014-12-04 15:54:34 +1300

    refractored viewmodel js, built Bookshelf js and html references, but something is horribly wrong with it

--- SrcCode/SharpBooks/SharpBooks/Scripts/Application/Bookshelf.js
+++ SrcCode/SharpBooks/SharpBooks/Scripts/Application/Bookshelf.js
@@ -1,1 +1,21 @@
-﻿
+﻿var BookshelfViewModel = function() {
+    var self = this;
+    self.bookshelfitems = ko.observableArray();
+
+    $.ajax({
+        url: 'api/BookShelf',
+        data: { search: $('#Search').val() },
+        type: 'GET',
+        datatType: 'json',
+        success: function (data) {
+            self.bookshelfitems(data);
+            console.log("bread & butter");
+        },
+        fail: function () {
+            alert('something up');
+        }
+
+    });
+}
+ko.applyBindings(new BookshelfViewModel());
+// GET: api/BookShelf
