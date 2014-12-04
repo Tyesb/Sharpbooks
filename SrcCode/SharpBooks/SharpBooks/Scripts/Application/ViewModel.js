99fc9c5db7b4734388d53a379711fb1eb5dbb3d0
gggmyyy <gmyoung321@gmail.com>
2014-12-03 19:35:21 -0800

    images now dislplay

--- SrcCode/SharpBooks/SharpBooks/Scripts/Application/ViewModel.js
+++ SrcCode/SharpBooks/SharpBooks/Scripts/Application/ViewModel.js
@@ -1,13 +1,13 @@
 ﻿console.log('onehittaquitta');
 var ClickCounterViewModel = function () {
-
+    this.searchBox = ko.observable(); //ko is the JS object that he knockout js library provies. searchbox = to what got enetere in 
     this.searchResults = ko.observableArray();
     var self = this;
     this.searchClick = function () {
 
         $.ajax({
             url: '/api/Search',
-            data: { search: $('#Search').val() },
+            data: { search: this.searchBox() },
             type: 'GET',
             datatType: 'json',
             success: function (data) {
