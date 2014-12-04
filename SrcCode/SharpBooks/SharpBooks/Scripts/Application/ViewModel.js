d224ddd8b61324fafc152a3dd8e42682a41c0ade
gggmyyy <gmyoung321@gmail.com>
2014-12-04 12:58:10 -0800

    add to bookshelf button registers but does nothing

--- SrcCode/SharpBooks/SharpBooks/Scripts/Application/ViewModel.js
+++ SrcCode/SharpBooks/SharpBooks/Scripts/Application/ViewModel.js
@@ -3,7 +3,7 @@ var ClickCounterViewModel = function () {
     this.searchBox = ko.observable(); //ko is the JS object that he knockout js library provies. searchbox = to what got enetere in 
     this.searchResults = ko.observableArray();
     var self = this;
-    this.searchClick = function () {
+    this.searchClick = function() {
 
         $.ajax({
             url: '/api/Search',
@@ -11,7 +11,30 @@ var ClickCounterViewModel = function () {
             type: 'GET',
             datatType: 'json',
             success: function (data) {
+                console.log("post list results", data);
+                data.forEach(function(book, index) {
+                    book.imageTitle = "Cover of: " + book.Title;
+                });
                 self.searchResults(data);
+            },
+            fail: function() {
+                alert('something up');
+            }
+
+        });
+    }
+
+    this.addToBookShelf = function (book) {
+        console.log("book is: ", book);
+      
+
+        $.ajax({
+            url: '/api/Bookshelf',
+            data: { ISBN: book.ISBN },
+            type: 'POST',
+            datatType: 'json',
+            success: function (data) {
+                console.log("post bookshelf results", data);
             },
             fail: function () {
                 alert('something up');
